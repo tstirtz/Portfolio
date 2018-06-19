39d06c751c73d1d243a8e6f505d799f9097d2dd4
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-06-18 20:25:06 -0700

    Fix top padding of section headers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onscroll = function(){
   const navBar = document.getElementsByTagName('nav')[0];
   const projects = document.querySelector('.projects'); 
-  if(projects.getBoundingClientRect().top <= 0){
+  if(projects.getBoundingClientRect().top <= 25){
     navBar.setAttribute('class', 'visible');
   }else{
     navBar.setAttribute('class', 'hidden');
